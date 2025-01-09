2b7c2dbb1a0e5d4922c2225ceab105fc5d467c39
GouthamZP <gouthamr@zero-pixels.com>
2025-01-09 15:08:38 +0530

    add cors

--- src/main.py
+++ src/main.py
@@ -1,15 +1,27 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .generate import generate_tests, PromptInput
 
 logging.basicConfig(level=logging.INFO)
 
+origins = ["*"]
+
+
 class GenerateRequestBody(PromptInput):
     pass
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/generate")
 async def generate(body: GenerateRequestBody):
