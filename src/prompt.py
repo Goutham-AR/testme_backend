f1eba2c638c4f3704d0312559983fd0f45a26d81
GouthamZP <gouthamr@zero-pixels.com>
2025-01-12 21:18:37 +0530

    update payload

--- src/prompt.py
+++ src/prompt.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class PromptInput(BaseModel):
     code: str
     extension: str
-    test_framework: str
+    testFramework: str
 
 class Import(BaseModel):
     path: str
@@ -15,7 +15,7 @@ class PromptInputWithImport(PromptInput):
 
 def generate_prompt(input: PromptInput):
     prompt = f"""
-        Generate unit test for the below given code assuming ${input.test_framework} library is installed. Cover all edge cases and generate only output code as text, no need of any prefix suffix or any extra note. If you want to include some notes, include it as comments in the generated code. Some informations about the code is given after the code.
+        Generate unit test for the below given code assuming ${input.testFramework} library is installed. Cover all edge cases and generate only output code as text, no need of any prefix suffix or any extra note. If you want to include some notes, include it as comments in the generated code. Some informations about the code is given after the code.
         code: {input.code}
         file extension: {input.extension}
     """
@@ -27,10 +27,10 @@ def generate_prompt_with_imports(input: PromptInputWithImport):
     for imp in input.imports:
         imports += f"""
             path: {imp.path}
-            content: {imp.code}
+            content: {imp.code},
         """
     prompt = f"""
-        Generate unit test for the below code assuming ${input.test_framework} library is installed. Cover all edge cases and generate only output code, no need of any prefix, suffix or any extra notes. If you want to include some notes, include it as comments in the generated code. The extension of the code file is {input.extension}. The code is given between '<code>'. The content of local imports in the given code is given alongside its path after the code segment.
+        Generate unit test for the below code assuming ${input.testFramework} library is installed. Cover all edge cases and generate only output code, no need of any prefix, suffix or any extra notes. If you want to include some notes, include it as comments in the generated code. The extension of the code file is {input.extension}. The code is given between '<code>'. The content of local imports in the given code is given alongside its path after the code segment. Each imports are separated by ','.
         <code>
         {input.code}
         <code>
