4c9a28bc1cb6e0a2005ba9ad219a022d0b0fefd5
GouthamZP <gouthamr@zero-pixels.com>
2025-01-12 23:29:43 +0530

    change model

--- src/config.py
+++ src/config.py
@@ -1,2 +1,2 @@
-# MODEL_NAME = "Qwen/Qwen2.5-Coder-7B-Instruct"
-MODEL_NAME = "Qwen/Qwen2.5-Coder-32B-Instruct"
+MODEL_NAME = "Qwen/Qwen2.5-Coder-7B-Instruct"
+# MODEL_NAME = "Qwen/Qwen2.5-Coder-32B-Instruct"
