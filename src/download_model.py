43fb1c1b764a5e041975b9acb65bf1a2f3177b23
GouthamZP <gouthamr@zero-pixels.com>
2025-01-14 11:21:53 +0530

    change model

--- src/download_model.py
+++ src/download_model.py
@@ -3,8 +3,6 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from .config import MODEL_NAME
 
 model = AutoModelForCausalLM.from_pretrained(
-    MODEL_NAME,
-    torch_dtype="auto",
-    device_map="auto"
-    )
+    MODEL_NAME, torch_dtype="auto", device_map="auto"
+)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
